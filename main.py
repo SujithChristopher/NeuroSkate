90113238d457ba946111bab2fd4857d5584d599f
SujithChristopher <chrisbon95@gmail.com>
2024-09-06 11:14:04 +0530

    changing camera to default camera

--- main.py
+++ main.py
@@ -128,7 +128,7 @@ class MainWindow(QMainWindow):
         self.vbox.addWidget(self.vtk_widget)
         self.vbox.addWidget(self.start_button)
         
-        self.capture = cv2.VideoCapture(1, cv2.CAP_DSHOW)
+        self.capture = cv2.VideoCapture(0, cv2.CAP_DSHOW)
         self.capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
         self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)        
         self.capture.set(cv2.CAP_PROP_FPS, 60)
